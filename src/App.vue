8d21cc60466a89fea941a91cccbc81689d4efef8
eXmatrx <matrx.eternal@gmail.com>
2023-06-08 12:48:56 +0200

    Add project files.

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <ManyWorld msg="Hello to many world"/>
+</template>
+
+<script>
+    import HelloWorld from './components/HelloWorld.vue'
+    import ManyWorld from "./components/ManyWorld.vue"
+
+    export default {
+        name: 'App',
+        components: {
+            HelloWorld,
+            ManyWorld
+        }
+    }
+</script>
+
+<style>
+    #app {
+        font-family: Avenir, Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #e5d9d9;
+        margin-top: 60px;
+    }
+
+    body {
+        background-color: black;
+    }
+</style>
