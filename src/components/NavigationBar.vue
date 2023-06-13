51a62eb4b4c5f1c9412294ba3fbe99b93afa551e
eXmatrx <matrx.eternal@gmail.com>
2023-06-13 15:34:34 +0200

    Added navigation support for mobile devices

--- src/components/NavigationBar.vue
+++ src/components/NavigationBar.vue
@@ -1,5 +1,12 @@
 <script setup>
     import navItems from "../assets/navbar.json";
+    import { ref } from 'vue'
+
+    const isNavbarShown = ref(false);
+    window.addEventListener('resize', function(e) {
+        if (window.innerWidth > 600 && isNavbarShown.value)
+            isNavbarShown.value = false;
+    });
 </script>
 
 <template>
@@ -11,8 +18,14 @@
                     <a :href="item.path">{{ item.name }}</a>
                 </li>
             </ul>
+            <input id="navbar-switcher" type="checkbox" @click="isNavbarShown = !isNavbarShown" :checked="isNavbarShown"/>
         </div>
     </div>
+    <label id="navbar-menu" v-if="isNavbarShown">
+        <li v-for="item in navItems" :key="item.name">
+            <a :href="item.path">{{ item.name }}</a>
+        </li>
+    </label>
 </template>
 
 <style scoped>
@@ -46,5 +59,46 @@
 
     #navbar-items li {
         margin-right: 10px;
+    }
+
+    #navbar-items a {
+        text-decoration: none;
+    }
+
+    #navbar-switcher {
+        width: 48px;
+        height: 48px;
+        margin-right: 24px;
+        display: none;
+    }
+
+    #navbar-menu {
+        width: 100%;
+        background-color: aquamarine;
+    }
+
+    #navbar-menu {
+        position: absolute;
+        list-style: none;
+    }
+
+    #navbar-menu a {
+        text-decoration: none;
+        width: 100%;
+        height: 48px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        background-color: yellow;
+    }
+
+    @media (max-width: 600px) { 
+        #navbar-items {
+            display: none;
+        }
+
+        #navbar-switcher {
+            display: block;
+        }
     }
 </style>
